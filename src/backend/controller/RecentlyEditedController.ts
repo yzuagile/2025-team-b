1b8ea7144c405fc14688b309c578b9788e6f85a6
johnchen0112 <johnchen930112@gmail.com>
2025-05-06 21:37:31 +0800

    feat: 新增ShowTitle file

--- src/backend/controller/RecentlyEditedController.ts
+++ src/backend/controller/RecentlyEditedController.ts
@@ -1,36 +1,36 @@
-import { NoteDataModel } from "../model/NoteDataModel";
+// import { NoteDataModel } from "../model/NoteDataModel";
 
-export class RecentlyController{
+// export class RecentlyController{
 
-    private DataModel:NoteDataModel;
+//     private DataModel:NoteDataModel;
 
-    constructor(){
-        // 用 mock 的方式先模擬 data model 的資料
-        this.DataModel = new NoteDataModel();
-    }
+//     constructor(){
+//         // 用 mock 的方式先模擬 data model 的資料
+//         this.DataModel = new NoteDataModel();
+//     }
 
-    public updateEditedNote(note_id:number):void{
+//     public updateEditedNote(note_id:number):void{
 
-        try{
-            this.DataModel.updateNoteTimestamp(note_id);
-        }
-        catch (err){
+//         try{
+//             this.DataModel.updateNoteTimestamp(note_id);
+//         }
+//         catch (err){
             
-            throw err;
-        }
-    }
+//             throw err;
+//         }
+//     }
 
-    public getRecentlyEditedNotes(): string[] {
-        /*
-        * 回傳排序最近使用時間（由近到久）後的 檔案名稱
-        */ 
+//     public getRecentlyEditedNotes(): string[] {
+//         /*
+//         * 回傳排序最近使用時間（由近到久）後的 檔案名稱
+//         */ 
 
-        const notes = this.DataModel.getAllNotes();
+//         const notes = this.DataModel.getAllNotes();
         
-        const sortedNotesByTime = Object.values(notes).sort((a, b) => {
-            return b.last_edit_time - a.last_edit_time;
-        });
+//         const sortedNotesByTime = Object.values(notes).sort((a, b) => {
+//             return b.last_edit_time - a.last_edit_time;
+//         });
         
-        return sortedNotesByTime.map(note => note.content_file_name)
-    }
-}
+//         return sortedNotesByTime.map(note => note.content_file_name)
+//     }
+// }
---
+++ src/backend/controller/ShowTitle.ts
@@ -0,0 +1,8 @@
+import * as NoteStructure from "../interfaces/NoteStructure";
+import * as FileManager from "../utils/FileManager"
+
+export function showTitle(Note: any): string {
+
+
+    return ''
+}
