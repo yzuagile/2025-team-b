541a1754ddbd9d8f6118baee818d716a21d504bf
johnchen0112 <johnchen930112@gmail.com>
2025-05-10 15:51:30 +0800

    fix: 更新 showTitle 函數的參數類型並移除返回值

--- src/backend/controller/ShowTitle.ts
+++ src/backend/controller/ShowTitle.ts
@@ -1,8 +1,8 @@
 import * as NoteStructure from "../interfaces/NoteStructure";
 import * as FileManager from "../utils/FileManager"
 
-export function showTitle(Note: any): string {
+export function showTitle(Note: NoteStructure.Note): void {
+    const TitleTime: [string, string] = ["", ""];
 
 
-    return ''
 }
