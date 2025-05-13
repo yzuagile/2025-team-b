3340de3c9f4846f985ed5cce71660d4c1029362b
johnchen0112 <johnchen930112@gmail.com>
2025-05-13 21:40:38 +0800

    fix: 移除不必要的導入，簡化 ShowTitle 測試檔案

--- __test__/ShowTitle.test.ts
+++ __test__/ShowTitle.test.ts
@@ -1,4 +1,3 @@
-import { isNull } from 'util';
 import { showTitle } from '../src/backend/controller/ShowTitle';
 import *as getNote from '../src/backend/model/get';
 import { NOTE_TEST_DATA } from './TestData';
