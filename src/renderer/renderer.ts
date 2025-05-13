d6021cef150fd4d931a8e02a913aefa62b07e70b
iPadBaby <154890715+iPadBaby@users.noreply.github.com>
2025-05-13 23:09:57 +0800

    fix:修改輸入方式以及初步完成新增筆記
    將新增筆記的輸入方式改回dialog，並初步完成新增筆記的呼叫

--- src/renderer/renderer.ts
+++ src/renderer/renderer.ts
@@ -3,17 +3,21 @@ const noteDialog = document.getElementById('noteDialog') as HTMLDialogElement;
 const okBtn = document.getElementById('okBtn') as HTMLButtonElement;
 const cancelBtn = document.getElementById('cancelBtn') as HTMLButtonElement;
 const noteTitleInput = document.getElementById('noteTitle') as HTMLInputElement;
+import * as createNote from "../backend/model/create";
+import * as get from "../backend/model/get";
 
 addNoteBtn.addEventListener('click', () => {
   noteTitleInput.value = 'untitle'; // reset to default
   noteDialog.showModal();
 });
 
-okBtn.addEventListener('click', (e) => {
+okBtn.addEventListener('click', async (e) => {
   e.preventDefault(); // prevent default form close
   const title = noteTitleInput.value.trim();
   if (title) {
-    console.log(`📝 新增筆記：${title}`);
+    let newNoteId = createNote.createNote();
+    const newNote = await get.getNote(newNoteId);
+    newNote.title = title;
   }
   noteDialog.close();
 });
