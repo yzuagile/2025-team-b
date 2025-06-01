cfdbdcb16f31adfff91dfb2b9c170584f5251ae5
jhaoen <jhaoen1106@gmail.com>
2025-06-01 22:31:22 +0800

    新增編輯儲存功能，切換筆記能載入內容

--- src/renderer/views/App.tsx
+++ src/renderer/views/App.tsx
@@ -6,8 +6,6 @@ interface AddNoteButtonProps {
   onCreate: (title: string, labels: string[], content: string) => Promise<void>;
 }
 
-function handleSelect(note_id: string) { }
-
 Modal.setAppElement("#root");
 
 function AddNoteButton({ onCreate }: AddNoteButtonProps) {
@@ -54,7 +52,10 @@ function AddNoteButton({ onCreate }: AddNoteButtonProps) {
 }
 
 export default function App() {
+
   const [notes, setNotes] = useState<Record<string, string>>({});
+  const [selectedNote, setSelected] = useState<Note>(undefined);
+  const [contextValue, setContextValue] = useState("");
 
   // 載入所有筆記
   useEffect(() => {
@@ -66,21 +67,59 @@ export default function App() {
     });
   }, []);
 
+  // 在 App 關閉時可以把變更的內容儲存
+  useEffect(() => {
+
+    const beforeUnload = async () => {
+      if (selectedNote) {
+        await window.noteAPI.updateContext(
+          selectedNote.note_id,
+          contextValue
+        );
+      }
+    };
+
+    // effect 執行時（mount 或 deps 變更後），把 listener 加到 window
+    window.addEventListener("beforeunload", beforeUnload);
+
+    // cleanup：在下一次 effect 之前，或 component unmount 時，把 listener 移除
+    return () => {
+      window.removeEventListener("beforeunload", beforeUnload);
+    };
+  }, [selectedNote]);
+
   async function handleCreateNote(
     title: string,
     label: string[],
     content: string
   ) {
+
     let newId = await window.noteAPI.createNote(title, label, content);
     if (!newId) return;
 
-    const note: Note = await window.noteAPI.getNote(newId);
     setNotes((prev) => ({
       ...prev,
-      [note.title]: note.note_id,
+      [title]: newId,
     }));
+    await handleSelect(newId);
   }
 
+  async function handleSelect(uuid: string) {
+
+    // 先把舊的編輯內容儲存
+    if (selectedNote) {
+      await window.noteAPI.updateContext(selectedNote.note_id, contextValue);
+    }
+
+    // 載入新的
+    const note = await window.noteAPI.getNote(uuid);
+    setSelected(note);
+    setContextValue(note.context || "");
+  }
+
+  async function handleEditorChange(context: string) {
+    setContextValue(context);
+  }
   return (
     <>
       <div className="notion-app">
@@ -89,7 +128,7 @@ export default function App() {
           <AddNoteButton onCreate={handleCreateNote}></AddNoteButton>
           <ul className="tag-list">
             {Object.entries(notes).map(([title, id]) => (
-              <li key={title} className="tag-item" onClick={() => handleSelect(id)}>
+              <li key={title} className={`tag-item${id === selectedNote?.note_id ? " selected" : ""}`} onClick={() => handleSelect(id)}>
                 {title}
               </li>
             ))}
@@ -98,7 +137,12 @@ export default function App() {
         {/* 編輯區 */}
         <div className="editor">
           <div className="editor-title"></div>
-          <textarea className="editor-area" placeholder="請輸入筆記內容..." />
+          <textarea 
+            className="editor-area" 
+            placeholder="請輸入筆記內容..." 
+            value={contextValue}
+            onChange={(e) => handleEditorChange(e.target.value)} 
+          />
         </div>
       </div>
     </>
