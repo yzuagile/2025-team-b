07cdf67d5913963230532ee7d83b86b3d598220a
jhaoen <jhaoen1106@gmail.com>
2025-06-10 17:01:02 +0800

    去除不必要的程式和修改

--- src/renderer/modes/OrderListMode.ts
+++ src/renderer/modes/OrderListMode.ts
@@ -2,24 +2,21 @@ import { TextMode } from "./TextMode";
 
 export class OrderListMode extends TextMode {
 
-    actionPerform(start: number, end: number): string|undefined {
+    actionPerform(start: number, end: number): string | undefined {
 
         if (this.value.slice(start, end + 1).length) {
+
             let sel = this.value.substring(start, end + 1);
             let insert: string;
 
-            if (sel.length > 0) {
-                const lines = sel.split("\n");
-                insert = lines
-                    .map((line, i) => `${i + 1}. ${line}`)
-                    .join("\n");
-            }
-
-            return this.value.slice(0, start) + insert + this.value.slice(end);
+            const lines = sel.split("\n");
+            insert = lines
+                .map((line, i) => `${i + 1}. ${line}`)
+                .join("\n");
+            return this.value.slice(0, start) + insert + this.value.slice(-end);
         }
-        else{
+        else {
             return undefined;
         }
-
     }
 }
