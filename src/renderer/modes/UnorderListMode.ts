7e629e04bea61e9d9a1e13d8df6f73fff2f618e5
jhaoen <jhaoen1106@gmail.com>
2025-06-10 17:03:00 +0800

    modify UnorderListMode

--- src/renderer/modes/UnorderListMode.ts
+++ src/renderer/modes/UnorderListMode.ts
@@ -8,10 +8,8 @@ export class UnorderListMode extends TextMode {
             let sel = this.value.substring(start, end + 1);
             let insert: string;
 
-            if (sel.length > 0) {
-                const lines = sel.split("\n");
+          const lines = sel.split("\n");
                 insert = lines.map(line => `- ${line}`).join("\n");
-            }
 
             return this.value.slice(0, start) + insert + this.value.slice(end);
         }
